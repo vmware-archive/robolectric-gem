41b7ac212e17ae642487814b840f382bb96cccbd
Mike Mazur & Tyler Schultz <pair+mmazur+tyler@pivotallabs.com>
2011-09-02 14:45:19 -0700

    Add ActivityMatcher#toHaveStarted()

--- src/robolectricgem/com/pivotallabs/robolectricgem/matchers/ActivityMatcher.java
+++ src/robolectricgem/com/pivotallabs/robolectricgem/matchers/ActivityMatcher.java
@@ -1,11 +1,24 @@
 package com.pivotallabs.robolectricgem.matchers;
 
 import android.app.Activity;
+import android.content.Intent;
 import com.pivotallabs.greatexpectations.MatcherOf;
+import com.xtremelabs.robolectric.shadows.ShadowIntent;
+
+import static com.xtremelabs.robolectric.Robolectric.shadowOf;
 
 @MatcherOf(Activity.class)
 public class ActivityMatcher<T extends Activity, M extends ActivityMatcher<T, M>> extends AndroidMatcher<T, M> {
     public boolean toBeFinishing() {
         return actual.isFinishing();
+    }
+
+    public boolean toHaveStarted(Class<? extends Activity> expected) {
+        Intent nextStartedActivity = shadowOf(actual).getNextStartedActivity();
+        if (nextStartedActivity == null) {
+            return false;
+        }
+        ShadowIntent shadowIntent = shadowOf(nextStartedActivity);
+        return shadowIntent.getComponent().getClassName().equals(expected.getName());
     }
 }
--- test/java/com/pivotallabs/robolectricgem/matchers/ActivityMatcherTest.java
+++ test/java/com/pivotallabs/robolectricgem/matchers/ActivityMatcherTest.java
@@ -1,6 +1,7 @@
 package com.pivotallabs.robolectricgem.matchers;
 
 import android.app.Activity;
+import android.content.Intent;
 import com.pivotallabs.greatexpectations.GreatExpectations;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.Before;
@@ -28,10 +29,18 @@ public class ActivityMatcherTest {
         expect(matcher.toBeFinishing()).toBeTrue();
     }
 
+    @Test
+    public void test_toHaveStarted() throws Exception {
+        expect(matcher.toHaveStarted(FooActivity.class)).toBeFalse();
+        activity.startActivity(new Intent(activity, FooActivity.class));
+        expect(matcher.toHaveStarted(FooActivity.class)).toBeTrue();
+    }
+
     private <T extends Activity> ActivityMatcher<T, ?> newActivityMatcher(T value) {
         ActivityMatcher matcher = new ActivityMatcher();
         GreatExpectations.setActual(matcher, value);
         return matcher;
     }
 
+    private class FooActivity extends Activity {}
 }
