a71cbfc0616cf16d7c140b66043676de4eec3942
Ryan Richard <pair+rrichard@pivotallabs.com>
2012-01-27 12:14:24 -0800

    Make it easier to subclass RunnableExpectGenerator

--- src/robolectricgem/com/pivotallabs/robolectricgem/expect/RunnableExpectGenerator.java
+++ src/robolectricgem/com/pivotallabs/robolectricgem/expect/RunnableExpectGenerator.java
@@ -13,6 +13,7 @@ import com.pivotallabs.robolectricgem.matchers.ViewMatcher;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
@@ -23,9 +24,14 @@ import java.util.List;
  * Run this class to regenerate Expect.java.
  *
  * This class is designed to be subclassed, in case you want to extend
- * the list of matcher classes with your own matcher classes.
+ * the list of matcher classes with your own matcher classes. The
+ * subclass can provide its own main() method, which can create an
+ * instance of itself and then call generateCustomExpect().
+ * Override methods to change file path or add matcher classes.
  */
 public class RunnableExpectGenerator extends ExpectGenerator {
+
+    private String packageName;
 
     @SuppressWarnings({"unchecked"})
     protected static final Class<? extends BaseMatcher>[] CUSTOM_MATCHER_CLASSES = new Class[]{
@@ -41,14 +47,23 @@ public class RunnableExpectGenerator extends ExpectGenerator {
 
     public RunnableExpectGenerator(String packageName) {
         super(packageName);
+        this.packageName = packageName;
     }
 
-    public static void main(String args[]) throws java.io.IOException {
-        generateCustomExpect();
+    public static void main(String args[]) throws IOException {
+        new RunnableExpectGenerator(getExpectClassPackageName()).generateCustomExpect();
     }
 
-    public static void generateCustomExpect() throws FileNotFoundException {
-        String packageName = getExpectClassPackageName();
+    protected static String getExpectClassPackageName() {
+        return Expect.class.getPackage().getName();
+    }
+
+    protected String getPackageName() {
+        return packageName;
+    }
+
+    public void generateCustomExpect() throws FileNotFoundException {
+        String packageName = getPackageName();
         String path = getExpectClassFilePath();
         System.out.println("path = " + path);
         System.out.println("packagename = " + packageName);
@@ -65,14 +80,14 @@ public class RunnableExpectGenerator extends ExpectGenerator {
         return classes;
     }
 
-    protected static String getExpectClassPackageName() {
-        return Expect.class.getPackage().getName();
+    protected String getExpectClassFilePrefix() {
+        return "src" + File.separator + "robolectricgem" + File.separator;
     }
 
-    protected static String getExpectClassFilePath() {
-        return "src" + File.separator +
-                "robolectricgem" + File.separator +
-                getExpectClassPackageName().replace(".", File.separator) + File.separator +
+    protected String getExpectClassFilePath() {
+        return getExpectClassFilePrefix() +
+                getPackageName().replace(".", File.separator) +
+                File.separator +
                 "Expect.java";
     }
 }
